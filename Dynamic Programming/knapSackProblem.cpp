cf60ff628f07434202a80c5dd5c2ecdb30e24af1
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2025-02-10 18:08:05 +0530

    temp

--- Dynamic Programming/knapSackProblem.cpp
+++ Dynamic Programming/knapSackProblem.cpp
@@ -1,80 +1,75 @@
 #include <iostream>
-#include <cstdlib>
 #include <vector>
 using namespace std;
-class KnapSack
-{
+
+class KnapSack {
 public:
-    int Recurrsive(vector<int> const weight, vector<int> const values, int index, int capacity)
-    {
-        if (index == 0)
-        {
-            if (weight[0] <= capacity)
-                return values[0];
-            else
-                return 0;
+    int Recurrsive(const vector<int>& weight, const vector<int>& values, int index, int capacity) {
+        if (index == 0) {
+            return (weight[0] <= capacity) ? values[0] : 0;
         }
 
         int inclusive = 0;
-        if (weight[index] <= capacity)
+        if (weight[index] <= capacity) {
             inclusive = values[index] + Recurrsive(weight, values, index - 1, capacity - weight[index]);
+        }
         int exclusive = Recurrsive(weight, values, index - 1, capacity);
 
         return max(inclusive, exclusive);
     }
-    int Memoization(vector<int> const weight, const vector<int> values, int index, int capacity, vector<vector<int>> &mappingVector)
-    {
-        if (index == 0)
-            if (weight[0] <= capacity)
-                return values[0];
-            else
-                return 0;
-        if (mappingVector[index][capacity] != -1)
-            return mappingVector[index][capacity];
-        int inclusive = 0;
-        if (weight[index] <= capacity)
-            inclusive = values[index] + Memoization(weight, values, index - 1, capacity - weight[index], mappingVector);
-        int exclusive = Memoization(weight, values, index - 1, capacity, mappingVector);
 
-        mappingVector[index][capacity] = max(inclusive, exclusive);
-        return mappingVector[index][capacity];
+    int Memoization(const vector<int>& weight, const vector<int>& values, int index, int capacity, vector<vector<int>>& dp) {
+        if (index == 0) {
+            return (weight[0] <= capacity) ? values[0] : 0;
+        }
+        if (dp[index][capacity] != -1) {
+            return dp[index][capacity];
+        }
+
+        int inclusive = 0;
+        if (weight[index] <= capacity) {
+            inclusive = values[index] + Memoization(weight, values, index - 1, capacity - weight[index], dp);
+        }
+        int exclusive = Memoization(weight, values, index - 1, capacity, dp);
+
+        dp[index][capacity] = max(inclusive, exclusive);
+        return dp[index][capacity];
     }
 
-    int Tabulation(const vector<int> weights, const vector<int> values, int index, int capacity)
-    {
-        vector<vector<int>> mappingVector(index + 1, vector<int>(capacity + 1, -1));
-        for (int weight = weights[0]; weight <= capacity; weight++)
-        {
-            if (weights[0] <= capacity)
-                mappingVector[0][weight] = values[0];
-            else
-                mappingVector[0][weight] = 0;
+    int Tabulation(const vector<int>& weights, const vector<int>& values, int n, int capacity) {
+        vector<vector<int>> dp(n, vector<int>(capacity + 1, 0));
+
+        for (int w = weights[0]; w <= capacity; ++w) {
+            dp[0][w] = values[0];
         }
-        for (int i = 1; i < index; i++)
-        {
-            for (int weight = 0; weight <= capacity; weight++)
-            {
-                int inclusive = values[i] + mappingVector[i - 1][weight - weights[i]];
-                int exclusive = mappingVector[i - 1][weight];
-                mappingVector[i][weight] = max(inclusive, exclusive);
+
+        for (int i = 1; i < n; ++i) {
+            for (int w = 0; w <= capacity; ++w) {
+                int inclusive = (w >= weights[i]) ? values[i] + dp[i - 1][w - weights[i]] : 0;
+                int exclusive = dp[i - 1][w];
+                dp[i][w] = max(inclusive, exclusive);
             }
         }
-        return mappingVector[index - 1][capacity];
+
+        return dp[n - 1][capacity];
     }
 };
-int main()
-{
+
+int main() {
     vector<int> weight = {1, 3, 5, 7};
     vector<int> values = {1, 4, 2, 7};
     int capacity = 13;
     KnapSack ks;
+
     int max_value = ks.Recurrsive(weight, values, weight.size() - 1, capacity);
     cout << "Maximum value in Knapsack = " << max_value << endl;
-    vector<vector<int>> mappingVector(weight.size(), vector<int>(capacity + 1, -1));
-    int max_value_memo = ks.Memoization(weight, values, weight.size() - 1, capacity, mappingVector);
+
+    vector<vector<int>> dp(weight.size(), vector<int>(capacity + 1, -1));
+    int max_value_memo = ks.Memoization(weight, values, weight.size() - 1, capacity, dp);
     cout << "Maximum value in Knapsack with Memoization = " << max_value_memo << endl;
-    ks.Tabulation(weight, values, weight.size(), capacity);
+
     int max_value_tab = ks.Tabulation(weight, values, weight.size(), capacity);
     cout << "Maximum value in Knapsack with Tabulation = " << max_value_tab << endl;
+
     return EXIT_SUCCESS;
 }
