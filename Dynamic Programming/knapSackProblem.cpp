737e8948032a715c4c7271606540ae03f73b3b9c
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2025-02-10 19:03:03 +0530

    knapsack problem

--- Dynamic Programming/knapSackProblem.cpp
+++ Dynamic Programming/knapSackProblem.cpp
@@ -2,15 +2,19 @@
 #include <vector>
 using namespace std;
 
-class KnapSack {
+class KnapSack
+{
 public:
-    int Recurrsive(const vector<int>& weight, const vector<int>& values, int index, int capacity) {
-        if (index == 0) {
+    int Recurrsive(const vector<int> &weight, const vector<int> &values, int index, int capacity)
+    {
+        if (index == 0)
+        {
             return (weight[0] <= capacity) ? values[0] : 0;
         }
 
         int inclusive = 0;
-        if (weight[index] <= capacity) {
+        if (weight[index] <= capacity)
+        {
             inclusive = values[index] + Recurrsive(weight, values, index - 1, capacity - weight[index]);
         }
         int exclusive = Recurrsive(weight, values, index - 1, capacity);
@@ -18,16 +22,20 @@ public:
         return max(inclusive, exclusive);
     }
 
-    int Memoization(const vector<int>& weight, const vector<int>& values, int index, int capacity, vector<vector<int>>& dp) {
-        if (index == 0) {
+    int Memoization(const vector<int> &weight, const vector<int> &values, int index, int capacity, vector<vector<int>> &dp)
+    {
+        if (index == 0)
+        {
             return (weight[0] <= capacity) ? values[0] : 0;
         }
-        if (dp[index][capacity] != -1) {
+        if (dp[index][capacity] != -1)
+        {
             return dp[index][capacity];
         }
 
         int inclusive = 0;
-        if (weight[index] <= capacity) {
+        if (weight[index] <= capacity)
+        {
             inclusive = values[index] + Memoization(weight, values, index - 1, capacity - weight[index], dp);
         }
         int exclusive = Memoization(weight, values, index - 1, capacity, dp);
@@ -36,15 +44,19 @@ public:
         return dp[index][capacity];
     }
 
-    int Tabulation(const vector<int>& weights, const vector<int>& values, int n, int capacity) {
+    int Tabulation(const vector<int> &weights, const vector<int> &values, int n, int capacity)
+    {
         vector<vector<int>> dp(n, vector<int>(capacity + 1, 0));
 
-        for (int w = weights[0]; w <= capacity; ++w) {
+        for (int w = weights[0]; w <= capacity; ++w)
+        {
             dp[0][w] = values[0];
         }
 
-        for (int i = 1; i < n; ++i) {
-            for (int w = 0; w <= capacity; ++w) {
+        for (int i = 1; i < n; ++i)
+        {
+            for (int w = 0; w <= capacity; ++w)
+            {
                 int inclusive = (w >= weights[i]) ? values[i] + dp[i - 1][w - weights[i]] : 0;
                 int exclusive = dp[i - 1][w];
                 dp[i][w] = max(inclusive, exclusive);
@@ -53,9 +65,49 @@ public:
 
         return dp[n - 1][capacity];
     }
+
+    int SpaceOptimized(const vector<int> &weights, const vector<int> &values, int n, int capacity)
+    {
+        vector<int> previous(capacity + 1, 0);
+        vector<int> current(capacity + 1, 0);
+
+        for (int w = weights[0]; w <= capacity; ++w)
+        {
+            previous[w] = values[0];
+        }
+
+        for (int i = 1; i < n; ++i)
+        {
+            for (int w = 0; w <= capacity; ++w)
+            {
+                int inclusive = (w >= weights[i]) ? values[i] + previous[w - weights[i]] : 0;
+                int exclusive = previous[w];
+                current[w] = max(inclusive, exclusive);
+            }
+            previous = current;
+        }
+
+        return previous[capacity];
+    }
+
+    int MoreOptimized(const vector<int> &weights, const vector<int> &values, int n, int capacity)
+    {
+        vector<int> dp(capacity + 1, 0);
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int w = capacity; w >= weights[i]; --w)
+            {
+                dp[w] = max(dp[w], values[i] + dp[w - weights[i]]);
+            }
+        }
+
+        return dp[capacity];
+    }
 };
 
-int main() {
+int main()
+{
     vector<int> weight = {1, 3, 5, 7};
     vector<int> values = {1, 4, 2, 7};
     int capacity = 13;
@@ -70,6 +122,12 @@ int main() {
 
     int max_value_tab = ks.Tabulation(weight, values, weight.size(), capacity);
     cout << "Maximum value in Knapsack with Tabulation = " << max_value_tab << endl;
+
+    int max_value_space_opt = ks.SpaceOptimized(weight, values, weight.size(), capacity);
+    cout << "Maximum value in Knapsack with Space Optimization = " << max_value_space_opt << endl;
+
+    int max_value_more_opt = ks.MoreOptimized(weight, values, weight.size(), capacity);
+    cout << "Maximum value in Knapsack with More Optimization = " << max_value_more_opt << endl;
 
     return EXIT_SUCCESS;
 }
