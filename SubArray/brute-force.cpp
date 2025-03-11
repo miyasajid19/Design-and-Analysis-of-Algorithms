343ec9284b968b08b4f478546ef7167b2e37bd4e
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2025-03-11 09:01:30 +0530

    finds the maximum subarray sum

--- SubArray/brute-force.cpp
+++ SubArray/brute-force.cpp
@@ -3,16 +3,16 @@
 #include <vector>
 using namespace std;
 
-int maxSumSUbArray(vector<int>arr)
+int maxSumSUbArray(vector<int> arr)
 {
-    int MaxSum=INT_MIN;
-    for (int i=0;i<arr.size();i++)
+    int MaxSum = INT_MIN;
+    for (int i = 0; i < arr.size(); i++)
     {
-        int sum=0;
-        for (int j=i;j<arr.size();j++)
+        int sum = 0;
+        for (int j = i; j < arr.size(); j++)
         {
-            sum+=arr[j];
-            MaxSum=max(MaxSum,sum);
+            sum += arr[j];
+            MaxSum = max(MaxSum, sum);
         }
     }
     return MaxSum;
@@ -23,16 +23,16 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    cout << "Enter the number of elements in the array: "<<endl;
+    cout << "Enter the number of elements in the array: " << endl;
     int n;
     cin >> n;
     vector<int> arr(n);
-    cout << "Enter the elements of the array: "<<endl;    
+    cout << "Enter the elements of the array: " << endl;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-cout<<"maximum sum of subarray is: "<<maxSumSUbArray(arr);
+    cout << "maximum sum of subarray is: " << maxSumSUbArray(arr);
 
     return EXIT_SUCCESS;
 }
