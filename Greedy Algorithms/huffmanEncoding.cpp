00e4a73ae0114fab633931751685c17e5d486510
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2025-03-11 17:09:21 +0530

    huffmanEncoding.cpp

--- Greedy Algorithms/huffmanEncoding.cpp
+++ Greedy Algorithms/huffmanEncoding.cpp
@@ -6,13 +6,15 @@
 #include <unordered_map>
 using namespace std;
 
-class Node {
+class Node
+{
 public:
-    char data;    // For storing character
-    int freq;     // For storing frequency
+    char data; // For storing character
+    int freq;  // For storing frequency
     Node *left, *right;
 
-    Node(char d, int f) {
+    Node(char d, int f)
+    {
         this->data = d;
         this->freq = f;
         this->left = nullptr;
@@ -20,45 +22,54 @@ public:
     }
 };
 
-class Compare {
+class Compare
+{
 public:
-    bool operator()(Node* a, Node* b) {
+    bool operator()(Node *a, Node *b)
+    {
         return a->freq > b->freq;
     }
 };
 
-class Solution {
+class Solution
+{
 public:
-    void traverse(Node* root, unordered_map<char, string>& ans, string temp) {
-        if (root == nullptr) return;
-        
+    void traverse(Node *root, unordered_map<char, string> &ans, string temp)
+    {
+        if (root == nullptr)
+            return;
+
         // If it's a leaf node, store the character and its encoding.
-        if (root->left == nullptr && root->right == nullptr) {
+        if (root->left == nullptr && root->right == nullptr)
+        {
             ans[root->data] = temp;
         }
-        
+
         // Traverse the left and right child with updated string
         traverse(root->left, ans, temp + '0');
         traverse(root->right, ans, temp + '1');
     }
 
-    unordered_map<char, string> huffmanEncoding(string s, vector<int> f, int n) {
-        priority_queue<Node*, vector<Node*>, Compare> pq;
+    unordered_map<char, string> huffmanEncoding(string s, vector<int> f, int n)
+    {
+        priority_queue<Node *, vector<Node *>, Compare> pq;
 
         // Create the priority queue from the frequencies
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             pq.push(new Node(s[i], f[i]));
         }
 
         // Build the Huffman Tree
-        while (pq.size() > 1) {
-            Node* left = pq.top();
+        while (pq.size() > 1)
+        {
+            Node *left = pq.top();
             pq.pop();
 
-            Node* right = pq.top();
+            Node *right = pq.top();
             pq.pop();
 
-            Node* newNode = new Node('$', left->freq + right->freq);  // '$' is a placeholder
+            Node *newNode = new Node('$', left->freq + right->freq); // '$' is a placeholder
             newNode->left = left;
             newNode->right = right;
 
@@ -66,7 +77,7 @@ public:
         }
 
         // The root node of the Huffman Tree
-        Node* root = pq.top();
+        Node *root = pq.top();
 
         unordered_map<char, string> ans;
         traverse(root, ans, "");
@@ -75,7 +86,8 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     // Sample input
     string s = "abcdef";
     vector<int> f = {5, 9, 12, 13, 16, 45};
@@ -85,7 +97,8 @@ int main() {
     unordered_map<char, string> result = solution.huffmanEncoding(s, f, n);
 
     // Output the Huffman codes
-    for (auto& p : result) {
+    for (auto &p : result)
+    {
         cout << p.first << ": " << p.second << endl;
     }
 
