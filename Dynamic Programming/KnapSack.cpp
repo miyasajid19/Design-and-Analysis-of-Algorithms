ef37ab777040ab5515af4a4802e6ec2af6d06da9
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2025-03-12 06:28:59 +0530

    recurrsive solution

--- Dynamic Programming/KnapSack.cpp
+++ Dynamic Programming/KnapSack.cpp
@@ -3,36 +3,59 @@
 #include <algorithm>
 using namespace std;
 
-class Solution {
+class Solution
+{
     vector<int> weights;
     vector<int> values;
     int n;
 
 public:
-    Solution(const vector<int>& weights, const vector<int>& values, int n)
+    Solution(const vector<int> &weights, const vector<int> &values, int n)
         : weights(weights), values(values), n(n) {}
 
-    int knapSack(int capacity) {
+    int non_recurrsive(int capacity)
+    {
         vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
 
-        for (int i = 1; i <= n; ++i) {
-            for (int w = 1; w <= capacity; ++w) {
-                if (weights[i - 1] <= w) {
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int w = 1; w <= capacity; ++w)
+            {
+                if (weights[i - 1] <= w)
+                {
                     dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
-                } else {
+                }
+                else
+                {
                     dp[i][w] = dp[i - 1][w];
                 }
             }
         }
         return dp[n][capacity];
     }
+
+    // recurrsive
+    int recurrsive(int index, int capacity)
+    {
+        if (index == 0)
+            return weights[0] <= capacity ? values[0] : 0;
+
+        int inclusive = 0;
+        if (weights[index] <= capacity)
+            inclusive = values[index] + recurrsive(index - 1, capacity - weights[index]);
+
+        int exclusive = recurrsive(index - 1, capacity);
+        return max(inclusive, exclusive);
+    }
 };
 
-int main() {
+int main()
+{
     vector<int> weights = {1, 3, 4, 5};
     vector<int> values = {1, 4, 5, 7};
     int n = weights.size();
     Solution s(weights, values, n);
-    cout << "Maximum value in Knapsack = " << s.knapSack(7) << endl;
+    cout << "Maximum value in Knapsack = " << s.non_recurrsive(7) << endl;
+    cout << "Maximum value in Knapsack = " << s.recurrsive(n, 7) << endl;
     return EXIT_SUCCESS;
 }
