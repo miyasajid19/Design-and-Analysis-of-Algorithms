6ea7a7ea9506a1c1a1c383ef37a516115a71d4eb
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2025-03-12 08:36:48 +0530

    maximal square

---
+++ Dynamic Programming/MaximalSquare.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+class solution
+{
+    vector<vector<int>> array;
+    int row, column;
+
+public:
+    solution(vector<vector<int>> &array) : array(array)
+    {
+        this->row = array.size();
+        this->column = array[0].size();
+    }
+
+    int recurrsive(int i, int j, int &maximumsquare)
+    {
+        if (i >= row || j >= column)
+        {
+            return 0;
+        }
+
+        int right = recurrsive(i, j + 1, maximumsquare);
+        int diagonal = recurrsive(i + 1, j + 1, maximumsquare);
+        int left = recurrsive(i + 1, j, maximumsquare);
+
+        if (array[i][j] == 1)
+        {
+            int ans = 1 + min(right, min(left, diagonal));
+            maximumsquare = max(maximumsquare, ans);
+            return ans;
+        }
+        else
+            return 0;
+    }
+
+    int maxSquare()
+    {
+        int maximumsquare = 0;
+        recurrsive(0, 0, maximumsquare);
+        return maximumsquare;
+    }
+};
+int main()
+{
+
+    vector<vector<int>> array = {{1, 0, 1, 0, 0},
+                                 {1, 0, 1, 1, 1},
+                                 {1, 1, 1, 1, 1},
+                                 {1, 0, 0, 1, 0}};
+    solution sol(array);
+    cout << sol.maxSquare() << endl;
+    return EXIT_SUCCESS;
+}
