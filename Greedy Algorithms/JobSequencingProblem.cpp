828c452269b948aa1ba8b8ce153a200acc6b5399
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2025-03-12 22:06:01 +0530

    Job Sequencing

--- Greedy Algorithms/JobSequencingProblem.cpp
+++ Greedy Algorithms/JobSequencingProblem.cpp
@@ -4,22 +4,25 @@
 using namespace std;
 
 // Structure to represent a job
-struct Job {
+class Job
+{
+public:
     int id;       // Job ID
     int deadline; // Job deadline
     int profit;   // Job profit
 
-    Job(int id, int deadline, int profit)
-        : id(id), deadline(deadline), profit(profit) {}
+    Job(int id, int deadline, int profit) : id(id), deadline(deadline), profit(profit) {}
 };
 
 // Comparison function to sort jobs based on profit in descending order
-bool compare(Job a, Job b) {
+bool compare(Job a, Job b)
+{
     return a.profit > b.profit;
 }
 
 // Function to perform job sequencing and return the maximum profit
-void jobSequencing(vector<Job>& jobs, int n) {
+void jobSequencing(vector<Job> &jobs, int n)
+{
     // Sort the jobs in decreasing order of profit
     sort(jobs.begin(), jobs.end(), compare);
 
@@ -33,12 +36,16 @@ void jobSequencing(vector<Job>& jobs, int n) {
     vector<int> result;
 
     // Traverse through all the jobs
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         // Find a free slot for this job (latest available slot)
-        for (int j = min(n - 1, jobs[i].deadline - 1); j >= 0; j--) {
-            if (!slots[j]) {
+        for (int j = min(n - 1, jobs[i].deadline - 1); j >= 0; j--)
+        {
+            if (!slots[j])
+            {
                 // If the slot is free, schedule the job
                 slots[j] = true;
+                cout << "Job " << jobs[i].id << " is scheduled at slot " << j << endl;
                 totalProfit += jobs[i].profit;
                 jobCount++;
                 result.push_back(jobs[i].id);
@@ -51,16 +58,18 @@ void jobSequencing(vector<Job>& jobs, int n) {
     cout << "Number of jobs scheduled: " << jobCount << endl;
     cout << "Total profit: " << totalProfit << endl;
     cout << "Job sequence: ";
-    for (int i = 0; i < jobCount; i++) {
+    for (int i = 0; i < jobCount; i++)
+    {
         cout << result[i] << " ";
     }
     cout << endl;
 }
 
-int main() {
+int main()
+{
     // Example input: 5 jobs
-    vector<Job> jobs = { Job(1, 4, 20), Job(2, 1, 100), Job(3, 1, 19),
-                          Job(4, 1, 27), Job(5, 3, 25) };
+    vector<Job> jobs = {Job(1, 2, 20), Job(2, 2, 15), Job(3, 1, 10),
+                        Job(4, 3, 5), Job(5, 1, 3)};
     int n = jobs.size();
 
     // Perform job sequencing
